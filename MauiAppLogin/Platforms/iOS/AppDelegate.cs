79ab9407e769ba4d4653081717d8b283a95e87ad
plabsjc <pedro.lucas.sjc@gmail.com>
2025-05-26 17:38:24 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppLogin/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiAppLogin;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
